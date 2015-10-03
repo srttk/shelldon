1024359098a0a11664eb0b652ec7ffef36d39ddf
Sarath <sarath.hacks@gmail.com>
2015-10-03 11:36:12 +0530

    Added essentials.sh

---
+++ essentials.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Essentials Tools And Utilities
+# Author 	: Sarath
+
+# Update Package List
+
+sudo apt-get update
+
+#Install Build Essentials
+
+sudo apt-get install build-essential
+
+
+# Install Ubuntu  Restricted Extras
+
+sudo apt-get install ubuntu-restricted-extras
